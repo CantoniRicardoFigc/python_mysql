0e4fc142c95d1f108a8d93a794c30f0a9f2697a4
CantoniRicardoFigc <cantoni.riccardo@iisgalvanimi.edu.it>
2023-12-04 13:01:29 +0000

    inizio

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import mysql.connector
 
+#configurazione per la connessione al database
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
   password=""
 )
 
+#cursore che ci permette di eseguire i comandi su mysql
 mycursor = mydb.cursor()
 
 mycursor.execute("CREATE DATABASE mydatabase")
