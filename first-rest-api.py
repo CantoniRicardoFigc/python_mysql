8e990125b2b702911c72eb3104990b1ff822b77d
CantoniRicardoFigc <cantoni.riccardo@iisgalvanimi.edu.it>
2023-12-18 12:55:23 +0000

    versione 03,04 complete, 05 parte 2

--- first-rest-api.py
+++ first-rest-api.py
@@ -20,8 +20,45 @@ def hello():
 def getAllData():
     mycursor.execute("SELECT * FROM CLASH_ROYALE.Clash_Unit")
     myresult = mycursor.fetchall()
-    result = [];
+    result = []
     for x in myresult:
-        print(x);
-        result.append(x);
+        print(x)
+        result.append(x)
     return result
+
+@app.route("/air_transport")
+def air_transport():
+    mycursor.execute("SELECT * FROM CLASH_ROYALE.Clash_Unit WHERE Transport = 'Air'")
+    air_units = mycursor.fetchall()
+    result = []
+    for i in air_units:
+        result.append(i)
+    return str(result)
+
+@app.route("/epic_units")
+def epicUnits():
+    mycursor.execute("SELECT * FROM CLASH_ROYALE.Clash_Unit WHERE Rarity = 'epic'")
+    epic_units = mycursor.fetchall()
+    result = []
+    for i in epic_units:
+        result.append(i)
+    return str(result)
+
+@app.route("/count")
+def count():
+    mycursor.execute("SELECT * FROM CLASH_ROYALE.Clash_Unit WHERE Count = 'x1'")
+    count = mycursor.fetchall()
+    result = []
+    for i in count:
+        result.append(i)
+    return str(result)
+
+@app.route("/card_cost")
+def cardCost():
+    mycursor.execute("SELECT * FROM CLASH_ROYALE.Clash_Unit WHERE Cost = '8'")
+    cost = mycursor.fetchall()
+    result = []
+    for i in cost:
+        result.append(i)
+    return str(result)
+    
